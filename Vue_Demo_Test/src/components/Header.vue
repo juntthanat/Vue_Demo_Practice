ec6f46ab6c84d68cad82fd0f7e7d8105816b2d9c
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-06-12 18:22:05 +0700

    Fix Margin Problem
    there is a margin in the body (index.html) therefore, we need to reset it.

---
+++ Vue_Demo_Test/src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+    <div class="test">
+        hello world2
+    </div>
+    <div class="hello-world">
+        hello world
+    </div>
+</template>
+
+<style>
+.hello-world{
+    background-color: red;
+    position: relative;
+    /* width: 50%;
+    height: 50vh;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 50%); */
+    right: 60px;
+}
+.test{
+    background-color: blue;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+</style>
